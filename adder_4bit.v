741c9b2055bf69226ff856a5c315de12debcf305
skzhu <skzhu@ad.engr.wisc.edu>
2024-02-25 22:50:11 -0600

    CLA 4 bit adder

--- adder_4bit.v
+++ adder_4bit.v
@@ -1,29 +1,28 @@
 /*
- * addsub_4bit.v
+ * adder_4bit.v
  * Shawn Zhu
  * ECE552
  */
-module addsub_4bit (Sum, A, B, sub);
+module adder_4bit (Sum, A, B, Cin, Cout);
 input [3:0] A, B; 	//Input values
-input sub; 		//add-sub indicator
 output [3:0] Sum; 	//sum output
+input Cin;
+output Cout;
 
 
-wire [3:0] B_comp;
 wire [3:0] C;
 wire [3:0] sum_res;
 wire ovfl_pos;
 wire ovfl_neg; //To indicate overflow
 wire [3:0] P, G; //to store propogate and generate signals
 
-assign B_comp = (sub == 1) ? ~B : B;	//Check whether B needs to be negated or not
 
 //Generate and Propogate signals
-assign G = A & B_comp;
-assign P = A ^ B_comp;
+assign G = A & B;
+assign P = A ^ B;
 
 //Carry look ahead logic
-assign C[0] = sub; 
+assign C[0] = Cin; 
 assign C[1] = G[0] | (P[0] & C[0]);
 assign C[2] = G[1] | (P[1] & G[0]) | (P[1] & P[0] & C[0]);
 assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] & C[0]);
@@ -32,13 +31,16 @@ assign C[3] = G[2] | (P[2] & G[1]) | (P[2] & P[1] & G[0]) | (P[2] & P[1] & P[0] 
 assign sum_res = P ^ C;
 
 //check if both inputs are positive
-assign ovfl_pos = ~A[3] & ~B_comp[3] & sum_res[3]; 
+assign ovfl_pos = ~A[3] & ~B[3] & sum_res[3]; 
 
 //check if both inputs are negative
-assign ovfl_neg = A[3] & B_comp[3] & ~sum_res[3]; 
+assign ovfl_neg = A[3] & B[3] & ~sum_res[3]; 
 
 //deal with saturation
 assign Sum = (ovfl_pos) ? 4'b0111 : ((ovfl_neg) ?  4'b1000 : sum_res);
+
+//set a carry out value (will be necessary for 16 bit adder)
+assign Cout = G[3] | (P[3] & G[2]) | (P[3] & P[2] & G[1]) | (P[3] & P[2] & P[1] & G[0]) | (P[3] & P[2] & P[1] & P[0] & C[0]);
 
 
 endmodule
---
+++ addsub_4bit_tb.sv
@@ -0,0 +1,104 @@
+module addsub_4bit_tb();
+
+reg signed [3:0] stm_a,stm_b; // used as stimulus for a,b
+reg signed stm_cin;
+wire signed [3:0] add_mon, sub_mon; // used to monitor sum
+wire stm_cout_add, stm_cout_sub;
+
+
+adder_4bit DUT1(.Sum(add_mon), .A(stm_a), .B(stm_b), .Cin(stm_cin), .Cout(stm_cout_add));
+sub_4bit DUT2(.Sum(sub_mon), .A(stm_a), .B(stm_b), .Cin(stm_cin), .Cout(stm_cout_sub));
+
+int signed sum = 0;
+bit pos_overflow = 0;
+bit neg_overflow = 0;
+
+initial begin
+	//test add block
+	repeat(20) begin
+		// set inputs to be random
+		stm_a = $random;
+		stm_b = $random;
+		stm_cin = $random;
+	
+		pos_overflow = 0;
+		neg_overflow = 0; 
+       
+		sum = stm_a + stm_b - stm_cin;
+		
+		// display inputs
+		$display("Test Case %0d:", $time);
+		$display("A: %b, B: %b, Cin: %b", stm_a, stm_b, stm_cin);
+
+		//if the result cannot be representable in 4 bits then set overflow check
+		if (~stm_a[3] && ~stm_b[3] && sum[3]) 
+			pos_overflow = 1;
+		else if (stm_a[3] && stm_b[3] && ~sum[3]) // check if overflow considering decimal output not actual signed binary
+			neg_overflow = 1;
+		
+		#1
+		$display("Sum: %b", add_mon);
+
+		//Adder worked if the 4 bit result matches and the overflow was checked correctly
+		if (pos_overflow) begin 
+			if (add_mon != 7)
+				$error("Failed for pos_ovfl at time %t: A=%h, B=%h", $time, stm_a, stm_b);
+		end 
+		else if (neg_overflow) begin
+			if (add_mon != -8) 
+				$error("Failed for neg_ovfl at time %t: A=%h, B=%h", $time, stm_a, stm_b);
+		end 
+		else if (add_mon != sum[3:0])
+			$error("Failed at time %t: A=%h, B=%h", $time, stm_a, stm_b);
+			
+		
+
+	end
+	$display("All tests passed!");
+/**
+	//Test sub block
+	repeat(20) begin
+		// set inputs to be random
+		stm_a = $random;
+		stm_b = $random;
+		stm_cin = $random;
+	
+		pos_overflow = 0;
+		neg_overflow = 0; 
+      
+		sum = stm_a - stm_b + stm_cin;  
+		
+		// display inputs
+		$display("Test Case %0d:", $time);
+		$display("A: %b, B: %b, Cin: %b", stm_a, stm_b, stm_cin);
+
+		//if the result cannot be representable in 4 bits then set overflow check
+		if (sum < -8) 
+			neg_overflow = 1;
+		else if (sum > 7) // check if overflow considering decimal output not actual signed binary
+			pos_overflow = 1;
+		
+		#1
+		$display("Difference: %b", sub_mon);
+
+		//Adder worked if the 4 bit result matches and the overflow was checked correctly
+		if (pos_overflow) begin 
+			if (sub_mon != 7)
+				$error("Failed for pos_ovfl at time %t: A=%h, B=%h", $time, stm_a, stm_b);
+		end 
+		else if (neg_overflow) begin
+			if (sub_mon != -8) 
+				$error("Failed for neg_ovfl at time %t: A=%h, B=%h", $time, stm_a, stm_b);
+		end 
+		else if (sub_mon != sum)
+			$error("Failed at time %t: A=%h, B=%h", $time, stm_a, stm_b);
+			
+		
+
+	end
+	$display("All tests passed!");
+	**/
+end
+
+
+endmodule
