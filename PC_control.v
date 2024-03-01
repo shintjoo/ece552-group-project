715247df233649ac2aeea02f0f4848f5fd0b24b0
skzhu <skzhu@ad.engr.wisc.edu>
2024-02-29 21:43:08 -0600

    Added Control units

---
+++ PC_control.v
@@ -0,0 +1,45 @@
+module PC_control(C, I, F, PC_in, PC_out);
+input [3:0] C; //conditions from instruction
+input [9:0] I; //9 bit signed offset
+input [3:0] F; //flags from ALU
+input [16:0] PC_in; //un-incremented PC
+output reg [16:0] PC_out; //PC out
+
+wire [15:0] pc_branch;
+reg error;
+
+addsub_16bit branch(.Sum(pc_branch), .A(PC_in), .B({6'h0, I[8:0], 1'b0}), .sub(1'b0), .sat());
+
+always @ (C) begin
+    case(C) 
+        3'b000: begin
+            PC_out = (~F[1]) ?  pc_branch : PC_in;
+        end
+        3'b001: begin
+            PC_out = (~F[0] & F[1] & ~F[2]) ?  pc_branch : PC_in;
+        end
+        3'b010: begin
+            PC_out = (~F[0] & ~F[1]) ?  pc_branch : PC_in;
+        end
+        3'b011: begin
+            PC_out = (F[0] & ~F[1]) ?  pc_branch : PC_in;
+        end
+        3'b100: begin
+            PC_out = (F[1] || (~F[0] & ~F[1] )) ?  pc_branch : PC_in;
+        end
+        3'b101: begin
+            PC_out = (F[0] || F[1]) ?  pc_branch : PC_in;
+        end
+        3'b110: begin
+            PC_out = (F[2]) ?  pc_branch : PC_in;
+        end
+        3'b111: begin
+            PC_out =  pc_branch;
+        end
+        default: begin
+            error = 1; 
+        end
+    endcase
+end
+
+endmodule
--- cpu.v
+++ cpu.v
@@ -15,7 +15,7 @@ wire pcs_select, hlt_select, ALUSrc8bit;
 
 //intermediate signals
 wire [15:0] pc_in, instruction, mem_out;
-wire [15:0] pc_increment, pc_branch, pc_choose;
+wire [15:0] pc_increment, pc_branch; //, pc_choose;
 wire [15:0] datain, dataout1, dataout2;
 wire [2:0] Flags;
 wire [15:0] aluin2, aluout;
@@ -28,79 +28,22 @@ memory1c imem(.data_out(instruction), .data_in(16'b0), .addr(pc_in), .enable(1'b
 
 //PC Calculation
 addsub_16bit increment(.Sum(pc_increment), .A(pc_in), .B(16'h0002), .sub(1'b0), .sat());
-addsub_16bit branch(.Sum(pc_branch), .A(pc_increment), .B({6'h0, instruction[8:0], 1'b0}), .sub(1'b0), .sat());
+//addsub_16bit branch(.Sum(pc_branch), .A(pc_increment), .B({6'h0, instruction[8:0], 1'b0}), .sub(1'b0), .sat());
+PC_control pccontrol(.C(instruction[11:9]), .I(instruction[8:0]), .F(Flags), .PC_in(pc_increment), .PC_out(pc_branch));
 
-//Decode Stage
-//assign control signals
-// operate on 2 registers ADD 0000, SUB 0001, XOR 0010, RED 0011
-// operate on 2 registers part 2 PADDSB 0111
-// operate on one register and 4 bit imm SLL 0100, SRA 0101
-// operate on one register and 4 bit imm part 2 ROR 0110
-// operate on one register and 8 bit imm LLB 1010, LHB 1011
-// load LW 1000
-// store SW 1001
-// branch B 1100
-// branch register BR 1101
-// pcstore PCS 1110
-// halt HLT 1111
-always @ (instruction) begin
-    RegWrite  = 1'b0;
-    MemRead = 1'b0;
-    MemWrite = 1'b0;
-    Branch = 1'b0;
-    MemtoReg = 1'b0;
-    ALUSrc = 1'b0;
-    pcs_select = 1'b0;
-    hlt_select = 1'b0; 
-    ALUSrc8bit = 1'b0;  
-    error = 1'b0;
-	casex (instruction [3:0])
-    	4'b00xx: begin      // operate on 2 registers ADD 0000, SUB 0001, XOR 0010, RED 0011
-            RegWrite  = 1'b1;
-        end
-        4'b010x: begin      // operate on one register and 4 bit imm SLL 0100, SRA 0101
-            RegWrite  = 1'b1;
-            ALUSrc 1'b1;
-        end
-         4'b0110: begin      // operate on one register and 4 bit imm part 2 ROR 0110
-            RegWrite  = 1'b1;
-            ALUSrc 1'b1;
-        end
-        4'b0111: begin      // operate on 2 registers part 2 PADDSB 0111
-            RegWrite  = 1'b1;
-        end
-        4'b101x: begin      // operate on one register and 8 bit imm LLB 1010, LHB 1011
-            RegWrite  = 1'b1;
-            ALUSrc8bit = 1'b1;
-        end
-        4'b1000: begin      //lw
-            RegWrite  = 1'b1;
-            MemRead = 1'b1;
-            ALUSrc 1'b1;
-        end
-        4'b1001: begin      //sw
-            MemWrite = 1'b1;
-            ALUSrc 1'b1;
-        end
-        4'b1100: begin      // branch B 1100
-            Branch = 1'b1;
-        end
-        4'b1101: begin      // branch register BR 1101
-            Branch = 1'b0;
-        end
-        4'b1110: begin     // pcstore PCS 1110
-            pcs_select 1'b1;
-        end
-        4'b1111: begin      // halt HLT 1111
-            hlt_select 1'b1;  
-  
-        end
-        default: begin
-            error = 1;
-        end
-
-    endcase
-end
+//Control signals
+Control controlunit(
+    .instruction(instruction[3:0]), 
+    .RegWrite(RegWrite),       
+    .MemRead(MemRead),
+    .MemWrite(MemWrite),
+    .Branch(Branch),
+    .MemtoReg(MemtoReg),
+    .ALUSrc(ALUSrc),
+    .pcs_select(pcs_select),
+    .hlt_select(hlt_select),
+    .ALUSrc8bit(ALUSrc8bit)
+);
 
 //possibilities for opcode
 //Opcode rd, rs, rt
@@ -120,11 +63,11 @@ ALU ex(.ALU_Out(aluout), .Error(error), .ALU_In1(dataout1), .ALU_In2(aluin2), .A
 //memory stage
 memory1c dmem(.data_out(mem_out), .data_in(dataout2), .addr(aluout), .enable(MemRead), .wr(MemWrite), .clk(clk), .rst(rst));
 
-assign pc_choose = (Branch && ) pc_branch : pc_increment;
+//assign pc_choose = (Branch && ) pc_branch : pc_increment;
 
 //writeback stage
 assign datain = (pcs_select) ? pc_increment : ((MemtoReg) ? mem_out : aluout);
 
-assign pc = (hlt_select) ? pc_in : pc_choose;
+assign pc = (hlt_select) ? pc_in : pc_branch;
 
 endmodule
