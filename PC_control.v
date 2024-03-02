c00951533132c2ce4fe50ff1d9d504f56ea92bec
Shawn Zhu <shawnkzhu@gmail.com>
2024-03-02 00:55:51 -0600

    Found issue. pc_branch does not work

--- PC_control.v
+++ PC_control.v
@@ -7,40 +7,50 @@ input [15:0] PC_in; //un-incremented PC
 output reg [15:0] PC_out; //PC out
 
 wire [15:0] pc_branch;
-reg [15:0] pc_choose;
+reg [15:0] pc_choose, branch_imm;
 reg error;
 
-addsub_16bit branch_add(.Sum(pc_branch), .A(PC_in), .B({6'h0, I, 1'b0}), .sub(1'b0), .sat());
+addsub_16bit branch_add(.Sum(pc_branch), .A(PC_in), .B(branch_imm), .sub(1'b0), .sat());
 
 assign PC_out = pc_choose;
 
 always @ (C, I, F, PC_in) begin
     //default out
+    branch_imm = 16'h0000;
     pc_choose = 16'h0000;
     error = 0;
     case(C) 
         3'b000: begin
-            pc_choose = (branch) ? ((~F[1]) ?  pc_branch : PC_in) : PC_in ;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = (~F[1] && branch) ?  pc_branch : PC_in ;
+            //pc_choose = (branch) ?  PC_in : PC_in ;
         end
         3'b001: begin
-            pc_choose = (branch) ? ((~F[0] & F[1] & ~F[2]) ?  pc_branch : PC_in) : PC_in;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = (~F[0] && F[1] && ~F[2] && branch) ?  pc_branch : PC_in;
         end
         3'b010: begin
-            pc_choose = (branch) ? ((~F[0] & ~F[1]) ?  pc_branch : PC_in) : PC_in;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = (~F[0] && ~F[1] && branch) ?  pc_branch : PC_in;
         end
         3'b011: begin
-            pc_choose = (branch) ? ((F[0] & ~F[1]) ?  pc_branch : PC_in) : PC_in;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = (F[0] && ~F[1] && branch) ?  pc_branch : PC_in;
         end
         3'b100: begin
-            pc_choose = (branch) ? ((F[1] || (~F[0] & ~F[1] )) ?  pc_branch : PC_in) : PC_in;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = ((F[1] || (~F[0] && ~F[1])) && branch) ?  pc_branch : PC_in;
         end
         3'b101: begin
-            pc_choose = (branch) ? ((F[0] || F[1]) ?  pc_branch : PC_in) : PC_in;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = ((F[0] || F[1]) && branch) ?  pc_branch : PC_in;
         end
         3'b110: begin
-            pc_choose = (branch) ? ((F[2]) ?  pc_branch : PC_in) : PC_in;
+            branch_imm = 16'h0000 | (I << 1);
+            pc_choose = (F[2] && branch) ?  pc_branch : PC_in;
         end
         3'b111: begin
+            branch_imm = 16'h0000 | (I << 1);
             pc_choose =  pc_branch;
         end
         default: begin
