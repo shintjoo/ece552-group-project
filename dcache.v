d6a5a6a84683f0c78550fa262685964ac9480d72
skzhu <skzhu@ad.engr.wisc.edu>
2024-04-29 13:44:38 -0500

    icache and dcache

---
+++ dcache.v
@@ -0,0 +1,17 @@
+module dcache(clk, rst, MetaDataIn, MetaWrite, BlockEnable, MetaDataOut, DataIn, DataWrite);
+    input clk, rst;
+    input [7:0] MetaDataIn;
+    input MetaWrite;
+    input [127:0] BlockEnable;
+    output [7:0] MetaDataOut;
+    input [15:0] DataIn;
+    input DataWrite;
+    input [7:0] WordEnable;
+    output [15:0] DataOut;
+
+
+    MetaDataArray(.clk(clk), .rst(rst), .DataIn(MetaDataIn), .Write(MetaWrite), .BlockEnable(BlockEnable), .DataOut(MetaDataOut));
+
+    DataArray(.clk(clk), .rst(rst), .DataIn(DataIn), .Write(DataWrite), .BlockEnable(BlockEnable), .WordEnable(WordEnable), .DataOut(DataOut));
+
+endmodule
